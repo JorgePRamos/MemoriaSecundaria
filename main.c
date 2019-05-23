17c6aa3ceb6b8bf647abdf8ab7ff71a35e5bcfd3
Jorge Pérez Ramos <jorgeperezramos99@gmail.com>
2019-05-23 04:28:17 +0200

    Correcciones de iteración

--- main.c
+++ main.c
@@ -8,12 +8,9 @@
 int modificarReg(char *fichero, char *dni, char *provincia){
     FILE *f;
     tipoCubo cubo;
-    int y=0,x=0;
-    size_t numLee;
+    int y=0,x=0,bloqActual=0,found=0;
     int seed;
     int bloq;
-    int bloqActual = 0;
-    int found=0;
     //abrirArchivoFichero
     f = fopen(fichero,"rb");
     rewind(f);
@@ -24,37 +21,31 @@ int modificarReg(char *fichero, char *dni, char *provincia){
     //buscamos cubo
     fseek( f, sizeof(tipoCubo)*seed, SEEK_SET );
     fread( &cubo, sizeof(tipoCubo), 1, f );
-
-    if(cubo.numRegAsignados<=C){//no existe desborde
     for(y=0;y<C;y++){
         if(!strcmp(cubo.reg[y].dni, dni)){
             memcpy(&cubo.reg[y].provincia, provincia , sizeof(provincia));
+            fclose(f);
             return bloq;
         }
     }
-    return -1;//al no haber desvorde termiona la busqueda
-    }else if(cubo.numRegAsignados>C){//exixte desborde
-
-    while(found==0){
-        fseek( f, sizeof(tipoCubo)*(CUBOS+x), SEEK_SET );
-        fread( &cubo, sizeof(tipoCubo), 1, f );
-        for(y=0;y<C;y++){
-            if(!strcmp(cubo.reg[y].dni, dni)){
-                memcpy(&cubo.reg[y].provincia, provincia , sizeof(provincia));
-                return bloq;
+    if(cubo.numRegAsignados>C){//exixte desborde
+        while(found==0){
+            fseek( f, sizeof(tipoCubo)*(CUBOS+x), SEEK_SET );
+            fread( &cubo, sizeof(tipoCubo), 1, f );
+            for(y=0;y<C;y++){
+                if(!strcmp(cubo.reg[y].dni, dni)){
+                    memcpy(&cubo.reg[y].provincia, provincia , sizeof(provincia));
+                    fclose(f);
+                    return bloq;
+                }
             }
+            if(CUBOS+x>CUBOS+CUBOSDESBORDE){
+                fclose(f);
+                return-1;
+            }
+            x++;
         }
-        if(CUBOS+x>CUBOS+CUBOSDESBORDE){
-            return-1;
-        }
-        x++;
     }
-
-    }
-
-
-
-
 fclose(f);
-return 0; 
+return -1; //al no haber desvorde termiona la busqueda sin exito
 }
